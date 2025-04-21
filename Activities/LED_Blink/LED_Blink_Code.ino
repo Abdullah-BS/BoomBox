b51e3f6bacc1618dd09565990aa0713275f21581
Abdullah-BS <139412761+Abdullah-BS@users.noreply.github.com>
2025-04-21 17:53:31 +0300

    Create LED_Blink_Code.ino

---
+++ Activities/LED_Blink/LED_Blink_Code.ino
@@ -0,0 +1,21 @@
+// COMPLETE THE CODE!!
+
+int ledPin = #### ;  // ASSIGN THE PIN
+
+void setup() {
+
+  pinMode(ledPin, #### );  // INPUT or OUTPUT ??
+  
+  digitalWrite(ledPin, LOW); // start with LED off
+}
+
+void loop() {
+  
+  digitalWrite(ledPin, ####); // HIGH or LOW ??
+  
+  delay(1000);                // wait 1 s      1000 ms == 1 s
+  
+  digitalWrite(ledPin, LOW);  // HIGH or LOW ??
+  
+  delay(####);
+}
